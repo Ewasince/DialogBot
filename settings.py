9ad743f03393149be5d083c1081fb1978b48ac43
Ewasince <ewasince@gmail.com>
2022-07-14 12:46:38 +0300

    implements data acquisition

---
+++ settings.py
@@ -0,0 +1,3 @@
+token = 'vk1.a.fr4OtqfZ-6zOESOGHmTVGqvZ8AKmqIQS813EqYZvs85WOyX9G_QintKlTHwADn4ifCqE9eFh1TfX1AQnuoAZuMaWYYhnVUzXxFusmuA9u5Tu8LmF7I0-QMacZiEwvpzKTQzy4hEZdtq8CslvU6HjrqxSMjIwx9bihvqFqBT7R1HdkfvduA9_gwpLA0jl6aCb'
+confirmation_token = 'gfbv784beytivbe4ovt'
+access_token = '080deb67080deb67080deb672a08708a850080d080deb676ac089b6a9779b5921b8dd60'
